c541ebe4d1288b69d68769068b396397f3a1023a
KenanAsaad <kinan.bassam.assad@gmail.com>
2023-07-31 22:47:15 +0300

    feat(controller): create our first controller called VillaApiController

---
+++ Controllers/VillaApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learning.CoreApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaApiController : ControllerBase {
+    }
+}
